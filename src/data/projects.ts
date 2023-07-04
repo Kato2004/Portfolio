d0bc8880fced2522fe6083e22da913b489a412ce
Kato2004 <kauaalvesnascimento22@gmail.com>
2023-07-03 23:06:50 -0300

    added a new project and updated curriculum

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,6 +1,8 @@
 import imgProjectGame from "../images/jogo-da-velha-print.jpg";
 import moviesBank from "../images/moviedb.jpg";
 import todoListJpg from "../images/todo-list.jpg";
+import HqShopJpg from "../images/marvel-hq-shop.png";
+import LinkBasePng from "../images/linkbase-img.png";
 
 export const projects = [
   {
@@ -25,5 +27,21 @@ export const projects = [
       "To do com prioridade de execução e possibilidade de criar e excluir novas tarefas",
     tags: ["React", "Typescript"],
     title: "To do",
+  },
+  {
+    imgSrc: HqShopJpg,
+    projectSrc: "https://marvel-hq-shop-r9jan92bz-kato2004.vercel.app",
+    resume:
+      "Loja de quadrinhos virtual com visualização de carrinho de compras",
+    tags: ["React", "Typescript", "Cypress", "Styled Components"],
+    title: "Marvel HQ Store",
+  },
+  {
+    imgSrc: LinkBasePng,
+    projectSrc: "https://link-base-b789q8oc5-kato2004.vercel.app/",
+    resume:
+      "Projeto de um site salvar links com autenticação e possibilidade de adicionar, editar e excluir links",
+    tags: ["Next", "Typescript", "Tailwind", "Cypress"],
+    title: "LinkBase",
   },
 ];
